925e301b93ec5844bc52a35069e5d0fc7ab72e93
Sam Guns <sam.t.guns@gmail.com>
2021-12-11 20:22:14 +0000

    make backbone layers saveable

--- great-barrier-reef/backbone.py
+++ great-barrier-reef/backbone.py
@@ -6,20 +6,25 @@ import tensorflow_hub as hub
 class Backbone:
 
     # Start by downloading pretrained weights from the Tensorflow hub
-    def __init__(self):
+    def __init__(self, mode='classify', input_size=(128,128)):
 
         # Instantiate the feature extractor.
         # This is the feature of this class we will actually want to use
+        self.feature_model = hub.load("https://tfhub.dev/google/imagenet/inception_resnet_v2/feature_vector/5")
         self.feature_extractor = hub.KerasLayer(
-            "https://tfhub.dev/google/imagenet/inception_resnet_v2/feature_vector/5",
+            feature_model,
             trainable=True,
         )
 
+        self.thumbnail_detector = tf.keras.Sequential([self.feature_extractor])
+
+        if mode == 'classify':
         # TODO standardize the thumbansil sizes, I just hard coded in the feature sizes here and in data_utils.py
-        self.thumbnail_detector = tf.keras.Sequential(
-            [self.feature_extractor, tf.keras.layers.Dense(2, activation='softmax')]
-        )
-        self.thumbnail_detector.build([None, 128, 128, 3])
+          self.thumbnail_detector.add(
+              tf.keras.layers.Dense(1, activation=None)
+        ) 
+        input_h, input_w = input_size
+        self.thumbnail_detector.build([None, input_h, input_w, 3])
 
     def fine_tune(dataloader, epochs):
 
@@ -28,3 +33,6 @@ class Backbone:
         # We need to figure out what fine tuning parameters to use.
 
         pass
+
+    def save_backbone(self, path):
+        tf.saved_model.save(self.feature_model, path)
