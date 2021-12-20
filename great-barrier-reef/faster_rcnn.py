8a6d90722a7849bb37a774fb2bc574c9f44c908e
Neil Goeckner-Wald <ngoecknerwald@berkeley.edu>
2021-12-20 08:40:53 -0800

    Modicum of progress last night

--- great-barrier-reef/faster_rcnn.py
+++ great-barrier-reef/faster_rcnn.py
@@ -1,15 +1,30 @@
 # This script will contain the faster R-CNN class
 
-from rpn import RPN
-from backbone import Backbone
+import rpn, backbone
 
 
 class FasterRCNN:
-    def __init__(self):
+    def __init__(self, backbone='InceptionResNet-v2', rpn_kwargs={}):
+
+        '''
+        Instantiate the wrapper for the overall Faster R-CNN
+
+        backbone : str
+            Flavor of backone to use for feature extraction. Should
+            refer to  a subclass of Backbone()
+        rpn_kwargs : {}
+            Arguments to pass to the RPN, usually some set of hyperparameters.
+
+        '''
 
         # Instantiate components
-        self.backbone = Backbone()
-        self.rpn = RPN()
+        if backbone.lower() == 'inceptionresnet-v2':
+            self.backbone = Backbone_InceptionResNetV2()
+        else:
+            raise ValueError('Argument backbone=%s is not recognized.')
+
+        # Instantiate the RPN wrapper
+        self.rpn = RPNWrapper(**rpn_kwargs)
 
     def train(self, images_full, images_thumb):
         pass
